aa01c9e21f1e1c01c327788e2f46aec66ac4439a
karolk75 <karolk410410@gmail.com>
2022-06-25 20:00:39 +0000

    Update at 2022-06-25T20:00:39.099863Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,6 +7,6 @@ public class Main {
     }
 
     public static boolean numberEquals(String a, String b) {
-        return b.contains(a);
+        return Integer.parseInt(a) == Integer.parseInt(b);
     }
 }
