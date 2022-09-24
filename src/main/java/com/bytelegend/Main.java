65e0301c53e7ab9f21a9c6fcb5cd4967f16f1cea
YuanHao97 <y1997h@126.com>
2022-09-24 06:29:23 +0000

    Update at 2022-09-24T06:29:23.523901Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -2,11 +2,11 @@ package com.bytelegend;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(numberEquals("1234", "+1234"));
+        System.out.println(numberEquals("1234", "-1234"));
         System.out.println(numberEquals("1234", "1234"));
     }
 
     public static boolean numberEquals(String a, String b) {
-        return Integer.valueOf(a) == Integer.valueOf(b.replace("+",""));
+        return Integer.valueOf(a).equals(Integer.valueOf(b));
     }
 }
