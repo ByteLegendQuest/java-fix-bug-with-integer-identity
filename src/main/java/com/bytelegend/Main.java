3d6f342f51d37ebc52541efcded96ee38a827470
BasilGuo <basilguo@163.com>
2023-02-21 02:20:09 +0000

    Update at 2023-02-21T02:20:09.634998Z

--- src/main/java/com/bytelegend/Main.java
+++ src/main/java/com/bytelegend/Main.java
@@ -7,25 +7,6 @@ public class Main {
     }
 
     public static boolean numberEquals(String a, String b) {
-        int ia = 0, ib = 0;
-        int lena = a.length();
-        int lenb = b.length();
-
-        char[] aa = a.toCharArray();
-        char[] ab = b.toCharArray();
-
-        if (aa[0] == '+') {
-            ia = 1;
-        }
-        if (bb[0] == '+') {
-            ib = 1;
-        }
-
-        for (; ia < lena && ib < lenb; ia++, ib++) {
-            if (aa[ia] != ab[ib]) {
-                return false;
-            }
-        }
-        return true;
+        return Integer.valueOf(a) == Integer.valueOf(b);
     }
 }
